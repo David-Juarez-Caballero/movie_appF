cc88dd8c89b659eef98b50f442063e1f2f689f96
David <thedaviddraw@gmail.com>
2022-10-14 18:10:42 -0500

    Movie Page funcionando a medias

---
+++ src/pages/Movie.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {useParams} from "react-router-dom";
+import  {useFetchMovieById} from "../hooks/useFetchMovieById";
+import { MovieCard } from '../components/MovieCard';
+
+
+export const Movie = () => {
+
+    const {movieId} = useParams();
+    const {movie, isLoading} = useFetchMovieById(movieId);
+
+    console.log("Movie", movie);
+
+  return (
+    <div>
+        <MovieCard
+            {...movie}
+        />
+    </div>
+  )
+}
